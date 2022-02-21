6be4fca30fe5c024fa32408ea7897e60e015102c
jungleehyun <nickhyun@jungleehyuns-MacBook-Air.local>
2022-02-21 21:53:35 +0900

    init

---
+++ src/test/java/com/nicky/springGateWay/SpringGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicky.springGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
